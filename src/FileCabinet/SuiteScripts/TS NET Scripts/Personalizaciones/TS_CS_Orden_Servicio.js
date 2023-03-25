bf35c8200e0953ea09e7b953229cf5e6a1e652bb
Dennis Fernandez <dennisfernandez.issh@gmail.com>
2023-03-25 14:49:43 -0500

    Update comments script service order

--- src/FileCabinet/SuiteScripts/TS NET Scripts/Personalizaciones/TS_CS_Orden_Servicio.js
+++ src/FileCabinet/SuiteScripts/TS NET Scripts/Personalizaciones/TS_CS_Orden_Servicio.js
@@ -1,3 +1,11 @@
+/*=======================================================================================================================================================
+This script for service order
+=========================================================================================================================================================
+File Name: TS_CS_Orden_Servicio.js                                                                        
+Commit: 01                                                                                                                          
+Date: 19/03/2023
+Governance points: N/A
+========================================================================================================================================================*/
 /**
  *@NApiVersion 2.1
  *@NScriptType ClientScript
@@ -5,85 +13,36 @@
  */
 define(['N/search', 'N/currentRecord', 'N/ui/message'], (search, currentRecord, message) => {
     let typeMode = '';
-    let fieldBien = '';
 
     const pageInit = (scriptContext) => {
-        const objRecord = scriptContext.currentRecord;
         typeMode = scriptContext.mode; //!Importante, no borrar.
-        // console.log('Hola: ' + typeMode);
-        // let filtro = objRecord.getValue('custpage_field_tipo');
-        // console.log(filtro + ' - ' + filtro.length);
-        // fieldBien = form.addField({
-        //     id: 'custpage_field_bien',
-        //     type: serverWidget.FieldType.SELECT,
-        //     //source: 'customrecord_ht_record_bienes',
-        //     label: 'Bien',
-        //     container: 'fieldgroupid'
-        // });
-        // fieldBien.addSelectOption({ value: -1, text: '' });
-
-    }
-
-    function saveRecord(scriptContext) {
-
-    }
-
-    function validateField(scriptContext) {
-
     }
 
     const fieldChanged = (scriptContext) => {
         const objRecord = scriptContext.currentRecord;
         try {
-            if (objRecord.getValue({ fieldId: 'custpage_field_bien' }).length > 0) {
-                objRecord.setValue({ fieldId: 'custbody_ht_os_bien_flag', value: objRecord.getValue({ fieldId: 'custpage_field_bien' }), ignoreFieldChange: true })
-                // let searchBien = search.create({
-                //     type: "customrecord_ht_record_bienes",
-                //     filters: [["custrecord_ht_bien_propietario", "anyof", "502612"]], //terrestte, marítimo, inmueble, producción, tanque
-                //     columns: ['internalid', 'name']
-                // });
-
-                // searchBien.run().each((result) => {
-                //     var subId = result.getValue({ name: 'internalId' });
-                //     var subName = result.getValue({ name: 'name' });
-                //     fieldBien.addSelectOption({ value: subId, text: subName });
-                //     return true;
-                // });
+            if (typeMode == 'create' || typeMode == 'copy') {
+                if (objRecord.getValue({ fieldId: 'custbody_ht_so_bien' }) != objRecord.getValue({ fieldId: 'custbody_ht_os_bien_flag' })) {
+                    let linecount = parseInt(objRecord.getLineCount({ sublistId: 'item' }));
+                    if (linecount > 0) {
+                        console.log('Entré a borrar');
+                        for (let i = linecount - 1; i >= 0; i--) {
+                            objRecord.removeLine({ sublistId: 'item', line: i, ignoreRecalc: true });
+                        }
+                    }
+                    objRecord.setValue({ fieldId: 'custbody_ht_os_bien_flag', value: objRecord.getValue({ fieldId: 'custbody_ht_so_bien' }) })
+                }
             }
         } catch (error) {
             console.log(error)
         }
     }
 
-    function postSourcing(scriptContext) {
-
-    }
-
-    function lineInit(scriptContext) {
-
-    }
-
-    function validateDelete(scriptContext) {
-
-    }
-
-    function validateInsert(scriptContext) {
-
-    }
-
-    function validateLine(scriptContext) {
-
-    }
-
-    function sublistChanged(scriptContext) {
-
-    }
-
     return {
         pageInit: pageInit,
         // saveRecord: saveRecord,
         // validateField: validateField,
-        //fieldChanged: fieldChanged,
+        fieldChanged: fieldChanged,
         // postSourcing: postSourcing,
         // lineInit: lineInit,
         // validateDelete: validateDelete,
